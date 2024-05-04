3d9e67f62b070e7bdf6ee4eeac61f006ca000b82
ZhenhHuang <huangzhenhao_work@163.com>
2024-05-04 16:32:09 +0800

    Update layers.py

--- modules/layers.py
+++ modules/layers.py
@@ -10,9 +10,10 @@ class RSEncoderLayer(nn.Module):
         self.manifold = manifold
         self.fc = GCNConv(in_dim, out_dim)
         self.T = T
+        self.drop = nn.Dropout(0.0)
 
     def forward(self, x, edge_index):
-        x = self.fc(x, edge_index)
+        x = self.drop(self.fc(x, edge_index))
         z = self.manifold.expmap0(x)
         x = x.unsqueeze(0).repeat(self.T, 1, 1)
         return x, z
@@ -24,6 +25,7 @@ class RiemannianSGNNLayer(nn.Module):
         self.manifold = manifold
         self.layer = GCNConv(channels, channels, bias=False)
         self.neuron = RiemannianIFNode(manifold, v_threshold)
+        self.drop = nn.Dropout(0.0)
 
     def forward(self, s_seq, z_seq, edge_index):
         """
@@ -32,7 +34,7 @@ class RiemannianSGNNLayer(nn.Module):
         :param edge_index: [E, ]
         :return:
         """
-        x_seq = self.layer(s_seq, edge_index)
+        x_seq = self.drop(self.layer(s_seq, edge_index))
         y_seq = x_seq.mean(0)
         o_seq, z_seq = self.neuron(x_seq, y_seq, z_seq)
         return o_seq, z_seq
