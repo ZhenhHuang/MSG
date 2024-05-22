a6cabaa54cd0816d9611592e480f4032d5255bc1
ZhenhHuang <huangzhenhao_work@163.com>
2024-05-22 22:17:34 +0800

    Update run_nc.sh

--- scripts/run_nc.sh
+++ scripts/run_nc.sh
@@ -1,23 +1,5 @@
-python main.py --neuron "LIF" --task "NC" --dataset "computers" --manifold "lorentz"
+python main.py --neuron "IF" --task "NC" --dataset "Physics" --manifold "lorentz"
 
-python main.py --neuron "LIF" --task "NC" --dataset "computers" --manifold "sphere"
+python main.py --neuron "IF" --task "NC" --dataset "Physics" --manifold "sphere"
 
-python main.py --neuron "LIF" --task "NC" --dataset "computers" --manifold "euclidean"
-
-python main.py --neuron "LIF" --task "NC" --dataset "photo" --manifold "lorentz"
-
-python main.py --neuron "LIF" --task "NC" --dataset "photo" --manifold "sphere"
-
-python main.py --neuron "LIF" --task "NC" --dataset "photo" --manifold "euclidean"
-
-python main.py --neuron "LIF" --task "NC" --dataset "CS" --manifold "lorentz"
-
-python main.py --neuron "LIF" --task "NC" --dataset "CS" --manifold "sphere"
-
-python main.py --neuron "LIF" --task "NC" --dataset "CS" --manifold "euclidean"
-
-python main.py --neuron "LIF" --task "NC" --dataset "Physics" --manifold "lorentz"
-
-python main.py --neuron "LIF" --task "NC" --dataset "Physics" --manifold "sphere"
-
-python main.py --neuron "LIF" --task "NC" --dataset "Physics" --manifold "euclidean"
+python main.py --neuron "IF" --task "NC" --dataset "Physics" --manifold "euclidean"
